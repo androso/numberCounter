58881b7d06323fe80b9e4e0885f814d6fb73e7c5
androso <androso421@gmail.com>
2024-08-31 09:50:43 -0600

    chore: adding Number interface and classes for each type of number

---
+++ src/main/kotlin/common/Number.kt
@@ -0,0 +1,36 @@
+package org.homework.common
+
+interface BaseNumber {
+    val value: Int
+    fun printValue() {
+        println("The number is: $value")
+    }
+
+    fun checkType()
+}
+
+data class PrimeNumber(override val value: Int) : BaseNumber {
+    override fun checkType() {
+        println("The number $value is a prime number")
+    }
+}
+
+data class OddNumber(override val value: Int, val dividers: Array<Int>) : BaseNumber {
+    override fun checkType() {
+        println("The number $value is an odd number")
+    }
+
+    fun printDividers() {
+        println("The dividers of $value are: ${dividers.joinToString(", ")}")
+    }
+}
+
+data class EvenNumber(override val value: Int, val dividers: Array<Int>) : BaseNumber {
+    override fun checkType() {
+        println("The number $value is an even number")
+    }
+
+    fun printDividers() {
+        println("The dividers of $value are: ${dividers.joinToString(", ")}")
+    }
+}
